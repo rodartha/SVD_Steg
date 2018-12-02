6970c800c752b53979ead736459d1fb3403c19d2
Alecg956 <alechg@umich.edu>
2018-12-02 14:14:24 -0500

    SVD update

--- svd_steg/__main__.py
+++ svd_steg/__main__.py
@@ -11,13 +11,15 @@ from svd_steg.steganographer import Steganographer
 @click.option('--decode', '-d', default=False, is_flag=True, help='')
 @click.argument('message_file')
 @click.argument('image_file')
-def main(embed, decode, message_file, image_file):
+def main(embed, decode, image_file, message_file):
     """Run Steganographer tool."""
     method = ""
     if embed:
-        method = "encode"
+        print ("METHOD embed")
+        method = "embed"
     elif decode:
         method = "decode"
+        print ("METHOD decode")
 
     input_dir = "input"
 
@@ -25,6 +27,7 @@ def main(embed, decode, message_file, image_file):
     if not os.path.exists(input_dir):
         print("Error: Input Directory does not exist")
         exit(1)
+
     if not os.path.isfile(input_dir + '/' + image_file):
         print("Error: Image file "
               + image_file + " does not exist in input folder.")
@@ -35,11 +38,13 @@ def main(embed, decode, message_file, image_file):
     if not os.path.isfile(input_dir + '/' + message_file):
         print("Error: Message file "
               + message_file + " does not exit in input folder.")
+
     file = open(input_dir + '/' + message_file, 'r')
     message_in = file.read()
 
     # Run Steganography Tool
-    stego = Steganographer(method, image_in,
+    print(method)
+    stego = Steganographer(method,image_in,
                            image_file, message_in, message_file)
     stego.run()
 
--- svd_steg/steganographer.py
+++ svd_steg/steganographer.py
@@ -2,6 +2,7 @@
 import os
 import numpy
 import imageio
+from numpy import dot
 from svd_steg.helper import progress_bar
 
 
@@ -11,12 +12,17 @@ class Steganographer:
     def __init__(self, method, image_in,
                  image_file, message_in=None, message_file=None):
         """Initialize Variables."""
+
+        print("INITIALIZING Steganographer with method " + method)
+
         self.message = message_in
         self.message_file = message_file
         self.image = image_in.astype(numpy.int32)
         self.image_file = image_file
         self.method = method
         self.embedded_image = numpy.zeros(self.image.shape)
+
+        print (self.method)
 
     def output_embedded_image(self):
         """Ouput an embedded image as IMAGENAME_steg."""
@@ -43,9 +49,76 @@ class Steganographer:
         else:
             self.output_decoded_text()
 
+    def create_block_image(self, block_size):
+        """ Create blocks """
+        return None
+
+    def computeSVD(self, image_block):
+        """compute the SVD of a single image block (will add input later)"""
+
+        U,s,VT = numpy.linalg.svd(image_block)
+
+        # create blank m x n matrix
+        Sigma = numpy.zeros((image_block.shape[0], image_block.shape[1]))
+        # populate Sigma with n x n diagonal matrix
+        Sigma[:image_block.shape[1], :image_block.shape[1]] = numpy.diag(s)
+
+        '''
+        print(U)
+        print()
+        print(Sigma)
+        print()
+        print(VT)
+        print()
+        '''
+
+        # reconstruction
+        '''
+        B = U.dot(Sigma.dot(VT))
+
+        print()
+        print(B)
+        '''
+
+        return [U, Sigma, VT]
+
     def embed(self):
         """Embed message into an image."""
-        # TODO: Implement
+
+        # break image into blockSize
+
+        # loop
+            # through each block and compute svd
+            # embed message bits
+            # ensure U is orthogonal
+
+        # testing on a single block, with blockSize = n for now - ALEC
+        A = []
+        blockSize = 8
+
+        # create the nxn array
+        for i in range(blockSize):
+            A.append(numpy.arange(blockSize*i, blockSize*(i+1)))
+
+        A = numpy.array(A)
+
+        # compute the SVD
+        res = self.computeSVD(A)
+
+        print(res[0])
+        print()
+        print(res[1])
+        print()
+        print(res[2])
+
+        # reconstruction
+        '''
+        B = U.dot(Sigma.dot(VT))
+
+        print()
+        print(B)
+        '''
+
         return None
 
     def decode(self):
@@ -56,8 +129,10 @@ class Steganographer:
     def run(self):
         """Run Steganography class."""
         if self.method == "embed":
+            print("RUNNING steganographer with METHOD embed")
             self.embed()
         else:
+            print("RUNNING steganographer with METHOD decode")
             self.decode()
 
-        self.output()
+        #self.output()
